4e7ea35ee1d6a8e7e277f30620b03ff65d502739
Istiak Kashem Jisan <istiakkashem35@gmail.com>
2026-02-04 02:46:30 +0600

    implement google login

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -2,11 +2,10 @@ import LoginForm from "@/components/auth/LoginForm";
 import SocialLogins from "@/components/auth/SocialLogins";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 const LoginPage = async () => {
   const session = await auth.api.getSession({ headers: headers() });
-  if (session.user) return redirect("/");
+  // if (session.user) return redirect("/");
   return (
     <section className="h-screen grid place-items-center">
       <div className="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
--- components/auth/SocialLogins.jsx
+++ components/auth/SocialLogins.jsx
@@ -1,6 +1,13 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image";
 
 const SocialLogins = () => {
+  const handleSocialSignin = async (provider) => {
+    const res = await authClient.signIn.social({ provider });
+    console.log("Response after social login: ", res);
+  };
+
   return (
     <>
       <div className="text-center text-xs text-gray-500">or Signup with</div>
@@ -9,7 +16,10 @@ const SocialLogins = () => {
           <Image src="/fb.png" alt="facebook" width={40} height={40} />
           <span>Facebook</span>
         </button>
-        <button className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
+        <button
+          onClick={() => handleSocialSignin("google")}
+          className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
+        >
           <Image src="/google.png" alt="google" width={40} height={40} />
           <span>Google</span>
         </button>
