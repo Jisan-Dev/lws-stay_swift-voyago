1627ce5b65e6acd1e8e08e25f3bab6e837bee3d3
Istiak Kashem Jisan <istiakkashem35@gmail.com>
2026-02-08 02:44:29 +0600

    Refactor Search component to accept search parameters and enable dynamic input handling

--- components/search/Search.jsx
+++ components/search/Search.jsx
@@ -1,4 +1,55 @@
-const Search = ({ fromList }) => {
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+const Search = ({ fromList, destination, checkin, checkout }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [searchTerm, setSearchTerm] = useState({
+    destination: destination || "",
+    checkin: checkin || "",
+    checkout: checkout || "",
+  });
+
+  const [allowSearch, setAllowSearch] = useState(true);
+
+  const handleInputs = (e) => {
+    const { name, value } = e.target;
+
+    const state = { ...searchTerm, [name]: value };
+
+    if (
+      new Date(state.checkin).getTime() > new Date(state.checkout).getTime() ||
+      (!state.checkin && state.checkout) ||
+      (!state.checkout && state.checkin)
+    ) {
+      setAllowSearch(false);
+    } else {
+      setAllowSearch(true);
+    }
+
+    setSearchTerm(state);
+  };
+
+  const handleSearch = () => {
+    const params = new URLSearchParams(searchParams);
+
+    params.set("destination", searchTerm.destination);
+    if (searchTerm?.checkin && searchTerm?.checkout) {
+      params.set("checkin", searchTerm?.checkin);
+      params.set("checkout", searchTerm?.checkout);
+    }
+
+    if (pathname.includes("hotels")) {
+      router.replace(`${pathname}?${params.toString()}`);
+    } else {
+      router.replace(`/hotels?${params.toString()}`);
+    }
+  };
+
   return (
     <>
       <div className="lg:max-h-[250px] mt-6">
@@ -6,11 +57,17 @@ const Search = ({ fromList }) => {
           <div>
             <span>Destination</span>
             <h4 className="mt-2">
-              <select name="destination" id="destination">
+              <select
+                name="destination"
+                id="destination"
+                value={searchTerm.destination}
+                onChange={handleInputs}
+              >
+                <option value=""> Select a destination </option>
                 <option value="Bali">Bali</option>
-                <option value="Bali">Cox&#39;s Bazar</option>
-                <option value="Bali">Sylhet</option>
-                <option value="Bali">Saint Martin</option>
+                <option value="Cox">Cox&#39;s Bazar</option>
+                <option value="Sylhet">Sylhet</option>
+                <option value="Saint Martin">Saint Martin</option>
                 <option value="Bali">Bali</option>
               </select>
             </h4>
@@ -19,20 +76,34 @@ const Search = ({ fromList }) => {
           <div>
             <span>Check in</span>
             <h4 className="mt-2">
-              <input type="date" name="checkin" id="checkin" />
+              <input
+                type="date"
+                name="checkin"
+                id="checkin"
+                value={searchTerm.checkin}
+                onChange={handleInputs}
+              />
             </h4>
           </div>
 
           <div>
             <span>Checkout</span>
             <h4 className="mt-2">
-              <input type="date" name="checkout" id="checkout" />
+              <input
+                type="date"
+                name="checkout"
+                id="checkout"
+                value={searchTerm.checkout}
+                onChange={handleInputs}
+              />
             </h4>
           </div>
         </div>
       </div>
 
-      <button className="search-btn">🔍️ {fromList ? "Modify Search" : "Search"}</button>
+      <button onClick={handleSearch} disabled={!allowSearch} className="search-btn">
+        🔍️ {fromList ? "Modify Search" : "Search"}
+      </button>
     </>
   );
 };
